128dcf86f6b5675b5800db72a0eeb111249380e9
Asif Khan <asif118052002@gmail.com>
2023-01-12 22:50:07 +0530

    setting commit

--- website/website/settings.py
+++ website/website/settings.py
@@ -48,7 +48,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'website.shop',
+    'website',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
