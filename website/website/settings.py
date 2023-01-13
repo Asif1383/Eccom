85cb6aae380730a58e8c4b6c9d33ad6b23160f90
Asif Khan <asif118052002@gmail.com>
2023-01-13 12:06:33 +0530

    render first commit

--- website/website/settings.py
+++ website/website/settings.py
@@ -38,10 +38,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-08nbfynohb)i+($-8ov*z0sw-i=5c0&&iz-+hmrjt^0caoww_h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 
 # Application definition
